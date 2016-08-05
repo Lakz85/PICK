464f19d4e1af95f0eda3b26583c819d6e9e30b90
Lakz85 <kurakes@nate.com>
2016-08-05 12:46:43 +0900

    Add write form

--- app/src/main/java/com/pick/WriteBandActivity.java
+++ app/src/main/java/com/pick/WriteBandActivity.java
@@ -3,8 +3,14 @@ package com.pick;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Display;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 
@@ -13,27 +19,81 @@ import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
  */
 public class WriteBandActivity extends AppCompatActivity {
 
-    DiscreteSeekBar ageSlider;
-    Display display;
-    Point displaysize;
-    int width;
-    Paint paint;
-    int point = 0;
-    //Create a new bitmap that is the width of the screen
-    /*Bitmap bitmap = Bitmap.createBitmap(width, 100, Bitmap.Config.ARGB_8888);
-    //A new canvas to draw on.
-    Canvas canvas = new Canvas(bitmap);*/
+
+    //에디트 텍스트
+    private EditText editText;
+
+    //스피너에 뿌려질 Array 형식의 Data를 담은 Adapter 생성
+    private Spinner mSpinner = null;
+    // 아답터에 담겨질 스피너 아답터 생성
+    private ArrayAdapter<String> mSpinnerAdapter = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.join_band);
-        ageSlider = (DiscreteSeekBar) findViewById(R.id.ageSlider);
-        ageSlider.setNumericTransformer(new DiscreteSeekBar.NumericTransformer() {
-            @Override
-            public int transform(int value) {
-                return value * 10;
-            }
-        });
+        setContentView(R.layout.band_wrtie_form);
+
+        // toolBar
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.concert_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_alert);
+        //뒤로가기 이미지 보내주면 바꿀것.
+        //아직 뒤로가기 menifest 에도 설정 하지 않았음,
+        //왜 안바뀌지;;;@_@:;;;
+
+
+
+        //  에디트 텍스트 포커스 맞추기.
+        editText = (EditText) findViewById(R.id.written);
+        editText.requestFocus();
+
+
+        //스피너 부분
+        //장르 스피너
+        mSpinner = (Spinner) findViewById(R.id.spinner_genre);
+        mSpinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                (String[]) getResources().getStringArray(R.array.sp_genre));
+        //dropdown 모양 설정 부분.
+        mSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //스피너에 아답터를 연결
+        mSpinner.setAdapter(mSpinnerAdapter);
+
+        //지역 스피너
+        mSpinner = (Spinner) findViewById(R.id.spinner_si);
+        mSpinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                (String[])getResources().getStringArray(R.array.city));
+        mSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(mSpinnerAdapter);
+
+        //구 스피너
+        // 지역의 스피너 값에 따라 구의 내용이 바뀌어야 함,,
+        mSpinner = (Spinner) findViewById(R.id.spinner_gu);
+        mSpinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                (String[])getResources().getStringArray(R.array.gu));
+        mSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(mSpinnerAdapter);
+
+        //지원파트
+        mSpinner = (Spinner) findViewById(R.id.spinner_part);
+        mSpinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                (String[])getResources().getStringArray(R.array.candidate));
+        mSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(mSpinnerAdapter);
+
+        //에디트 텍스트 처음 시작되는 키보드 숨김
+        InputMethodManager immhide = (InputMethodManager) getSystemService(MainActivity.INPUT_METHOD_SERVICE);
+        immhide.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY,0);
+
+
     }
 
 }
